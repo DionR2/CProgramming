857699aae34d0a74fc30b03c24d3f99e1e36cbc0
Dion Richard <drichard3370@floridapoly.edu>
2025-08-20 10:51:03 -0400

    Create First_program.c

---
+++ First_program.c
@@ -0,0 +1,18 @@
+/*
+
+multi-line comment.
+8/20/2025
+Dion Richard
+the first program in c.
+
+*/
+
+// one line comment 
+
+// preprocessor directive 
+// stdio.h - standard input output
+// .h - header file
+#include <stdio.h>
+
+// create the main function 
+int main(void)
